2060e875ae12979ed3068e2fe55ba401e9cacc6e
pranoti <pranotikadam96@gmail.com>
2024-05-11 04:11:09 +0530

    added responsiveness to the landing page

--- Frontend/HealthHubApp/src/components/Navbar.jsx
+++ Frontend/HealthHubApp/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import logo from '../assets/blue-logo.png';
-import '../css/navbar.css';
+import '../css/Navbar.css';
 import { useState } from 'react';
 
 const Navbar = () => {
