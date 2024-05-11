855e65a167740a14867b05e6e06950a68b5f477a
bansalnmn <bansalnmn@gmail.com>
2024-05-11 22:07:09 +0530

    update signup route from branch

--- Frontend/HealthHubApp/src/routes/AllRoutes.jsx
+++ Frontend/HealthHubApp/src/routes/AllRoutes.jsx
@@ -3,6 +3,7 @@ import { BookAppointment } from "../pages/BookAppointment";
 import { Login } from "../pages/Login";
 import { HomePage } from "../pages/HomePage";
 import { Dashboard } from "../pages/Dashboard";
+import { Signup } from "../pages/Signup";
 
 const AllRoutes = () => {
     return (
@@ -11,6 +12,7 @@ const AllRoutes = () => {
             <Route path="/appointments" element={<BookAppointment />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
         </Routes>
     );
 };
